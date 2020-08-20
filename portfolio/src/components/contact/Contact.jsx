a13eac8a2b42199766bb1365631c695bd26eb76e
EdithOrt <eoc.diseno@gmail.com>
2020-08-19 21:57:33 -0500

    Adding animation contact

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -3,13 +3,54 @@ import './contact.css'
 
 function Contact ({setContact, setComeBack}){
     return(
-        <div className="container-contact">
-            <button onClick={() => {
-                setComeBack(true)
-                setContact(false)
-            }}> Regresar </button>
-            <p>contacto</p>
-        </div>
+        <div className="container-principal-contact">
+            <div className="container-contact-left">
+                <button onClick={() => {
+                    setComeBack(true)
+                    setContact(false)
+                }}> Regresar </button>
+            </div>
+                <div className="container-inputs-form">
+                    <h2>¿LISTO(A) PARA TRABAJAR JUTOS(AS)?</h2>
+                    <div className="row100">
+                        <div className="col">
+                            <div className="input-box">
+                                <input type="text" name="" required="required"></input>
+                                <span className="text">Nombre completo</span>
+                                <span className="line"></span>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="input-box">
+                                <input type="text" name="" required="required">
+                                </input>
+                                <span className="text">Correo electrónico</span>
+                                <span className="line"></span>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="input-box">
+                                <input type="tel" name="" required="required">
+                                </input>
+                                <span className="text">Teléfono</span>
+                                <span className="line"></span>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="input-box textarea">
+                                <textarea required="required"></textarea>
+                                <span className="text">Ingresa tu mensaje aquí</span>
+                                <span className="line"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row100">
+                        <div className="col">
+                            <input type="submit" value="Enviar"></input>
+                        </div>
+                    </div>
+                </div>
+            </div>
     )
 }
 
