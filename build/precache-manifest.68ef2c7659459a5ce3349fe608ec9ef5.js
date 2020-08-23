abafa9bb7b21b16d35a3fce3e57b9a44170fdc62
EdithOrt <eoc.diseno@gmail.com>
2020-08-22 23:17:42 -0500

    add gitignore files

---
+++ build/precache-manifest.68ef2c7659459a5ce3349fe608ec9ef5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c44491da7913969e6d4c74f0a2c6cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "131957fcd61b78521ac4",
+    "url": "/static/css/main.34f3b968.chunk.css"
+  },
+  {
+    "revision": "8d0e3778d47968c00aa2",
+    "url": "/static/js/2.35e1bf58.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.35e1bf58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131957fcd61b78521ac4",
+    "url": "/static/js/main.9c25b945.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "dce302c396f33de7f65b1450de0cdd07",
+    "url": "/static/media/EDITH-ORTIZ-CV.dce302c3.pdf"
+  },
+  {
+    "revision": "a4a7dfcfe1c6cbb8e25a936ad9a50141",
+    "url": "/static/media/edith.a4a7dfcf.png"
+  },
+  {
+    "revision": "d5081029bf414710bf117202fb534875",
+    "url": "/static/media/mockup-burgerqueen.d5081029.png"
+  },
+  {
+    "revision": "d59fd570f7f8a7234d4314562e5d6cd0",
+    "url": "/static/media/mockup-secret-love.d59fd570.png"
+  },
+  {
+    "revision": "3354b92c0b3c075b7397fdd311dc132e",
+    "url": "/static/media/tools.3354b92c.png"
+  }
+]);
